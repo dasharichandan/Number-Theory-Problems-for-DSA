5a36f58364309db8acc42b3c7220a27805662dfc
Hari Chandan Das <139703650+dasharichandan@users.noreply.github.com>
2024-04-03 19:21:23 +0530

    Add files via upload

---
+++ Numberofonebits.java
@@ -0,0 +1,22 @@
+//Given a positive integer N, print count numbr of 1 bits in it. 
+
+import java.util.*;
+public class Numberofonebits {
+    public static int  Number(int N){
+        int count=0;
+        while(N!=0){
+            count+=N & 1;
+            N>>=1;
+        }
+        return count; 
+    }
+    public static void main(String aargs[]){
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter a no to check the no contans how many 1 bits:");
+      int N=sc.nextInt();
+      System.out.print(Number(N));
+
+      sc.close();
+    }
+    
+}
