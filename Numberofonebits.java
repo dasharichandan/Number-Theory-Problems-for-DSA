882eba99ab4e1a02dcbf23137fe53021731f1902
Hari Chandan Das <139703650+dasharichandan@users.noreply.github.com>
2024-04-03 20:16:57 +0530

    Update Numberofonebits.java

--- Numberofonebits.java
+++ Numberofonebits.java
@@ -5,11 +5,13 @@ public class Numberofonebits {
     public static int  Number(int N){
         int count=0;
         while(N!=0){
-            count+=N & 1;
-            N>>=1;
+            if((N & 1)!=0){
+            count=count+1;
         }
-        return count; 
+        N>>=1;
     }
+    return count; 
+}
     public static void main(String aargs[]){
       Scanner sc=new Scanner(System.in);
       System.out.print("Enter a no to check the no contans how many 1 bits:");
